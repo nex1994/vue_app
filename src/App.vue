feb42cb767ccb478fb1be98d2f4c7086528049d0
nex1994 <bilicki1994@gmail.com>
2025-06-24 17:23:36 +0200

    setup deployment

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="root">
+    <div class="todoapp">
+      <h1 class="todoapp__title">todos</h1>
+      <div class="todoapp__content">
+        <header class="todoapp__header"><button type="button" class="todoapp__toggle-all"
+            data-cy="ToggleAllButton"></button>
+          <form><input data-cy="NewTodoField" type="text" class="todoapp__new-todo" placeholder="What needs to be done?"
+              value=""></form>
+        </header>
+        <section class="todoapp__main" data-cy="TodoList">
+          <div data-cy="Todo" class="todo"><label class="todo__status-label"><input data-cy="TodoStatus" type="checkbox"
+                class="todo__status"></label> <span data-cy="TodoTitle" class="todo__title">dfgd</span><button
+              type="button" class="todo__remove" data-cy="TodoDelete">×</button>
+            <div data-cy="TodoLoader" class="modal overlay">
+              <div class="modal-background has-background-white-ter"></div>
+              <div class="loader"></div>
+            </div>
+          </div>
+          <div data-cy="Todo" class="todo"><label class="todo__status-label"><input data-cy="TodoStatus" type="checkbox"
+                class="todo__status"></label> <span data-cy="TodoTitle" class="todo__title">dddrtetrf</span><button
+              type="button" class="todo__remove" data-cy="TodoDelete">×</button>
+            <div data-cy="TodoLoader" class="modal overlay">
+              <div class="modal-background has-background-white-ter"></div>
+              <div class="loader"></div>
+            </div>
+          </div>
+          <div data-cy="Todo" class="todo"><label class="todo__status-label"><input data-cy="TodoStatus" type="checkbox"
+                class="todo__status"></label> <span data-cy="TodoTitle" class="todo__title">hfd</span><button
+              type="button" class="todo__remove" data-cy="TodoDelete">×</button>
+            <div data-cy="TodoLoader" class="modal overlay">
+              <div class="modal-background has-background-white-ter"></div>
+              <div class="loader"></div>
+            </div>
+          </div>
+        </section>
+        <footer class="todoapp__footer" data-cy="Footer"><span class="todo-count" data-cy="TodosCounter">3 items
+            left</span>
+          <nav class="filter" data-cy="Filter"><a href="#/" class="filter__link selected"
+              data-cy="FilterLinkAll">All</a><a href="#/active" class="filter__link"
+              data-cy="FilterLinkActive">Active</a><a href="#/completed" class="filter__link"
+              data-cy="FilterLinkCompleted">Completed</a></nav><button disabled="" type="button"
+            class="todoapp__clear-completed" data-cy="ClearCompletedButton">Clear completed</button>
+        </footer>
+      </div>
+      <div data-cy="ErrorNotification" class="hidden notification is-danger is-light has-text-weight-normal"><button
+          data-cy="HideErrorButton" type="button" class="delete"></button><br><br><br><br></div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './assets/styles/index.scss';
 import App from './App.vue'
 
 createApp(App).mount('#app')
